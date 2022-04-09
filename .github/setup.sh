db621d657dfaf64c1640d01b6d9221fcd568a8d5
Joel Van Eenwyk <joel.vaneenwyk@gmail.com>
2022-04-08 20:34:09 -0700

    Migrate CI scripts to `.github` folder

--- .github/setup.sh
+++ .github/setup.sh
@@ -4,16 +4,16 @@ set -ex
 echo "CXX="$CXX
 echo "CC="$CC
 if [[ "$TRAVIS_OS_NAME" == "linux" && "$CXX" = "g++" ]]; then
-  $SUDO apt-get update
-  $SUDO apt-get install -y python3
-  $SUDO apt-get install -y python3-pip
-  $SUDO apt-get install python3-dev
-  $SUDO pip3 install -U wheel
-  $SUDO pip3 install -U setuptools
-  python3 setup.py install --user
-  python3 examples/pybullet/unittests/unittests.py --verbose
-  python3 examples/pybullet/unittests/userDataTest.py --verbose
-  python3 examples/pybullet/unittests/saveRestoreStateTest.py --verbose
+    $SUDO apt-get update
+    $SUDO apt-get install -y python3
+    $SUDO apt-get install -y python3-pip
+    $SUDO apt-get install python3-dev
+    $SUDO pip3 install -U wheel
+    $SUDO pip3 install -U setuptools
+    python3 setup.py install --user
+    python3 examples/pybullet/unittests/unittests.py --verbose
+    python3 examples/pybullet/unittests/userDataTest.py --verbose
+    python3 examples/pybullet/unittests/saveRestoreStateTest.py --verbose
 fi
 cmake . -DBUILD_PYBULLET=ON -G"Unix Makefiles" #-DCMAKE_CXX_FLAGS=-Werror
 make -j8
